062d6de7c303569b079408f86439d05800785ceb
myorang <eoeh92@naver.com>
2019-01-26 22:02:31 +0900

    example Event
    example EventTrigger

--- Assets/Script/EventTrigger.cs
+++ Assets/Script/EventTrigger.cs
@@ -1,10 +1,28 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
+[RequireComponent(typeof(BoxCollider))]
 public class EventTrigger : MonoBehaviour
 {
+    public List<EventableObject> m_eventlist;
+    public List<GameObject> m_eventlists;
+
+    private int m_NowPlayIdx;
+
     public void ActiveEvent()
     {
         print("touch!");
+
+        m_eventlist[0].Activate();
+        m_NowPlayIdx = 0;
+    }
+
+    public void NextEvent()
+    {
+        m_NowPlayIdx++;
+        if (m_eventlist.Count > m_NowPlayIdx)
+        {
+            m_eventlist[m_NowPlayIdx].Activate();
+        }
     }
 }
