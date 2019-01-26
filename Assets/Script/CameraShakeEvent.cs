e4242a959de6dc33b10cf8fb8267d2e6cd4f0f2d
zxhjvb <zxhjvb@gmail.com>
2019-01-26 21:21:02 +0900

    no message

---
+++ Assets/Script/CameraShakeEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShakeEvent : MonoBehaviour {
+
+    [Header("Reference Object")]
+    [SerializeField]
+    private Transform mCameraObject;
+
+    [Header("Shake Detail Data")]
+    [SerializeField]
+    private float mShakeDuringTime;
+    [SerializeField]
+    private float mMagnitued;
+    [SerializeField]
+    private float mShakeDeltaTime;
+
+    public void StartShake()
+    {
+
+    }
+
+  
+}
